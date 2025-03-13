cae81d72ea60901e3c1105257278d6ac20029731
Pavan Uthsara <uthsarapavan@gmail.com>
2025-03-13 17:19:21 +0530

    user dashboard v1.5.1 - add imageview with intent

--- app/src/main/java/com/example/mad_labexam2/DashboardActivity.kt
+++ app/src/main/java/com/example/mad_labexam2/DashboardActivity.kt
@@ -1,6 +1,9 @@
 package com.example.mad_labexam2
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class DashboardActivity : AppCompatActivity() {
@@ -8,5 +11,10 @@ class DashboardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
 
+        val checkButton1 : ImageView = findViewById(R.id.imageView1)
+        checkButton1.setOnClickListener{
+            val intent = Intent(this, CheckActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
