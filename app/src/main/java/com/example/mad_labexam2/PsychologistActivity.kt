49ba31592353d1e0f66aa269126518c762ffc5aa
Pavan Uthsara <uthsarapavan@gmail.com>
2025-03-20 12:11:34 +0530

    psychologist page v1.0

---
+++ app/src/main/java/com/example/mad_labexam2/PsychologistActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mad_labexam2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PsychologistActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_psychologists)
+    }
+}
