d6551e19fee21e798700ac5c008199a00f89d739
Pavan Uthsara <uthsarapavan@gmail.com>
2025-03-27 22:56:41 +0530

    v3.0

--- app/src/main/java/com/example/mad_labexam2/BookPsychologist.kt
+++ app/src/main/java/com/example/mad_labexam2/BookPsychologist.kt
@@ -5,15 +5,14 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-class PsychologistActivity : AppCompatActivity() {
+class BookPsychologist : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_psychologists)
+        setContentView(R.layout.activity_bookpsychologist)
 
-        val doctor1bookbtn: Button = findViewById(R.id.doctor1_book_btn)
-
-        doctor1bookbtn.setOnClickListener{
-            val intent = Intent(this, BookPsychologist::class.java)
+        val cancelBtn : Button = findViewById(R.id.button3)
+        cancelBtn.setOnClickListener{
+            val intent = Intent(this, PsychologistActivity::class.java )
             startActivity(intent)
         }
     }
